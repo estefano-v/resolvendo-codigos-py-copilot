0399ec8df6c203814b433d36491981a006dd80e0
estefano-v <166577273+estefano-v@users.noreply.github.com>
2024-07-13 01:39:20 +0000

    Testando os Codigos

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,17 +1,17 @@
 # Vamos solicitar como entrada dois numeros e depois vamos realizar uma operação simples entre eles.
 
-# Solicitando os dois números
+# Solicitando os dois números.
 
 num1 = int (input("Entre com o primeiro numero: "))
 
 num2 = int (input("entre com o segundo número: "))
 
-# Entrando com a escolha da operação
+# Entrando com a escolha da operação.
 
 operação = input("Entre com a operação que deseja estar realizando (+ ,-, *, /) : ")
 
-# Realizando a condição da operação escolhida
-# Caso a operação seja diferente das anteriores retorna inválida
+# Realizando a condição da operação escolhida.
+# Caso a operação seja diferente das anteriores retorna inválida.
 
 if operação == "+":
     print(num1 + num2)
